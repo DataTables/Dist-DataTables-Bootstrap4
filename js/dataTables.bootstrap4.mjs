c66d16eed38b85bccf1aba35c4d7fc68d57346db
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-04-20 09:42:32 +0100

    03b82601261c3eb776e4d2795f0f42d8762bb602 Build fix: Correct error whereby `$` could be reassigned in DateTime
    https://datatables.net/forums/discussion/76103
    
    a7e4d8079f04d084d39e3fece9e7a4301a7b30b8 Merge branch 'master' of github.com:DataTables/DataTablesSrc
    
    Sync to source repo @a7e4d8079f04d084d39e3fece9e7a4301a7b30b8

--- js/dataTables.bootstrap4.mjs
+++ js/dataTables.bootstrap4.mjs
@@ -2,8 +2,11 @@
  * ©2011-2017 SpryMedia Ltd - datatables.net/license
  */
 
-import $ from 'jquery';
+import jQuery from 'jquery';
 import DataTable from 'datatables.net';
+
+// Allow reassignment of the $ variable
+let $ = jQuery;
 
 
 /**
