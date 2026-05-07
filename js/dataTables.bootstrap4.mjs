6c624cfec07a508071eaa8e7c68f79e7f4d2f15d
Allan Jardine <allan@datatables.net>
2026-05-07 15:33:51 +0000

    Updating package for DataTables 3

--- js/dataTables.bootstrap4.mjs
+++ js/dataTables.bootstrap4.mjs
@@ -1,80 +1,63 @@
-/*! DataTables Bootstrap 4 integration
+/*! DataTables Bootstrap 4 integration 3.0.0-beta.2
  * © SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
-import DataTable from 'datatables.net';
-
-// Allow reassignment of the $ variable
-let $ = jQuery;
-
-
-/**
- * DataTables integration for Bootstrap 4.
- *
- * This file sets the defaults and adds options to DataTables to style its
- * controls using Bootstrap. See https://datatables.net/manual/styling/bootstrap
- * for further information.
- */
+import DataTable, {Api, Dom, util} from 'datatables.net';
 
 /* Set the defaults for DataTables initialisation */
-$.extend( true, DataTable.defaults, {
-	renderer: 'bootstrap'
-} );
-
-
+DataTable.util.object.assignDeep(DataTable.defaults, {
+    renderer: 'bootstrap'
+});
 /* Default class modification */
-$.extend( true, DataTable.ext.classes, {
-	container: "dt-container dt-bootstrap4",
-	search: {
-		input: "form-control form-control-sm"
-	},
-	length: {
-		select: "custom-select custom-select-sm form-control form-control-sm"
-	},
-	processing: {
-		container: "dt-processing card"
-	},
-	layout: {
-		row: 'row justify-content-between',
-		cell: 'd-md-flex justify-content-between align-items-center',
-		tableCell: 'col-12',
-		start: 'dt-layout-start col-md-auto mr-auto',
-		end: 'dt-layout-end col-md-auto ml-auto',
-		full: 'dt-layout-full col-md'
-	}
-} );
-
-
+DataTable.util.object.assignDeep(DataTable.ext.classes, {
+    container: 'dt-container dt-bootstrap4',
+    search: {
+        input: 'form-control form-control-sm'
+    },
+    length: {
+        select: 'custom-select custom-select-sm form-control form-control-sm'
+    },
+    processing: {
+        container: 'dt-processing card'
+    },
+    layout: {
+        row: 'row justify-content-between',
+        cell: 'd-md-flex justify-content-between align-items-center',
+        tableCell: 'col-12',
+        start: 'dt-layout-start col-md-auto mr-auto',
+        end: 'dt-layout-end col-md-auto ml-auto',
+        full: 'dt-layout-full col-md'
+    }
+});
 /* Bootstrap paging button renderer */
 DataTable.ext.renderer.pagingButton.bootstrap = function (settings, buttonType, content, active, disabled) {
-	var btnClasses = ['dt-paging-button', 'page-item'];
-
-	if (active) {
-		btnClasses.push('active');
-	}
-
-	if (disabled) {
-		btnClasses.push('disabled')
-	}
-
-	var li = $('<li>').addClass(btnClasses.join(' '));
-	var a = $('<a>', {
-		'href': disabled ? null : '#',
-		'class': 'page-link'
-	})
-		.html(content)
-		.appendTo(li);
-
-	return {
-		display: li,
-		clicker: a
-	};
+    var btnClasses = ['dt-paging-button', 'page-item'];
+    if (active) {
+        btnClasses.push('active');
+    }
+    if (disabled) {
+        btnClasses.push('disabled');
+    }
+    var li = DataTable.Dom.c('li').classAdd(btnClasses.join(' '));
+    var a = DataTable.Dom
+        .c('a')
+        .attr('href', disabled ? null : '#')
+        .classAdd('page-link')
+        .html(content)
+        .appendTo(li);
+    return {
+        display: li.get(0),
+        clicker: a.get(0)
+    };
 };
-
 DataTable.ext.renderer.pagingContainer.bootstrap = function (settings, buttonEls) {
-	return $('<ul/>').addClass('pagination').append(buttonEls);
+    return DataTable.Dom
+        .c('ul')
+        .classAdd('pagination')
+        .append(buttonEls)
+        .get(0);
 };
 
 
 export default DataTable;
+export { Api, DataTable, Dom, util };
